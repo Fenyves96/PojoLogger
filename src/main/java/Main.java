40283d3920bf6e2149dbec0471dfa75034375b09
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2023-01-05 23:45:07 +0100

    console simulator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,54 @@
+import org.apache.commons.lang.StringUtils;
 import pojologger.logging.Logger;
 import pojologger.logging.LoggerImpl;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Logger logger = new LoggerImpl();
-        logger.setOutputFile("""
-                {"outPutFile" : "log1.txt"}
-                """
-        );
-        logger.setLogLevel("""
-        {"logLevel" : "warning"}
-        """);
+        String command = null;
+        while (!"X".equals(StringUtils.upperCase(command))) {
+            System.out.print("Command (F,L,A): ");
+            command = sc.next();
+            switch (StringUtils.upperCase(command)) {
+                case "F" -> setFileName(sc, logger);
+                case "L" -> setLogLevel(sc, logger);
+                case "A" -> addLog(sc, logger);
+            }
+        }
+    }
+
+    private static void addLog(Scanner sc, Logger logger) {
+        System.out.print("processId|message:");
+        String input = sc.next();
+        String[] processIdAndMessage = input.split(Pattern.quote("|"));
+        String processId = processIdAndMessage[0];
+        String message = processIdAndMessage[1];
+
         logger.addLog("""
                 {
-                "processId" : 123456,
-                "message" : "File Not Found"
+                "processId" : %s,
+                "message" : "%s"
                 }
-                """);
+                """.formatted(processId, message));
+    }
+
+    private static void setLogLevel(Scanner sc, Logger logger) {
+        System.out.print("log level: ");
+        String logLevel = sc.next();
+        logger.setLogLevel("""
+                {"logLevel" : "%s"}
+                """.formatted(logLevel));
+    }
+
+    private static void setFileName(Scanner sc, Logger logger) {
+        System.out.print("outPutFileName: ");
+        String outPutFileName = sc.next();
+        logger.setOutputFile("""
+                {"outPutFile" : %s}
+                """.formatted(outPutFileName));
     }
 }
